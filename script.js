ec96ef034d453a5bf3cf534927626173cb6cf3d8
eduardo7321 <eduardo.jose184@gmail.com>
2021-09-05 23:08:08 -0300

    add copy button

--- script.js
+++ script.js
@@ -2,10 +2,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector('#btn').addEventListener('click', function() {
         let text = document.querySelector('#texto').value
-        document.querySelector("#result").innerHTML = upper(text)
+        document.querySelector('#result').innerHTML = upper(text)
+        //document.querySelector("#cop").value = copy(text)
     })
-
+  
     function upper(text) {
         return text.toUpperCase()
     }
+
+   /* document.querySelector('#btn').addEventListener('click', function() {
+        let text = document.querySelector('#texto').value
+        document.querySelector('#result2').innerHTML = upper(text)
+        //document.querySelector("#cop").value = copy(text)
+    })*/
+
+
+
+
+
+    let copyTextareaBtn = document.querySelector('#botaocopy')
+
+    copyTextareaBtn.addEventListener('click', function(event) {
+      let copyTextarea = document.querySelector('.texto')
+      copyTextarea.select()
+    
+      try {
+        let successful = document.execCommand('copy')
+        let msg = successful ? 'sim!' : 'não!'
+        alert('Texto copiado? ' + msg)
+      } catch (err) {
+        alert('Opa, Não conseguimos copiar o texto, é possivel que o seu navegador não tenha suporte, tente usar Crtl+C.');
+      }
+    })
+
+    //<textarea class="textarea">Vamos copiar este texto?</textarea>
+    //<br>
+    //<button class="copiar">Copiar Texto</button>
+
 })
